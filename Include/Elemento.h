a797937834b478bda6947415330b71faa575d916
Luisa <luisa.lacave@grad.iprj.uerj.br>
2025-07-14 01:14:49 -0300

    Classe Abstrata Elemento

--- Include/Elemento.h
+++ Include/Elemento.h
@@ -1,25 +1,31 @@
 #ifndef ELEMENTO_H
 #define ELEMENTO_H
 
-// Classe abstrata usada como base para todos os tipos de dados nas estruturas
+// Aqui iremos criar a classe abstrata com nome "Elemento" com um atributo ID.
 class Elemento {
 protected:
-    int id;
+    int id; // Atributo protegido, de nome ID, como chave primária.
+            // Seu acesso só é permitido via getID().
 
 public:
-    Elemento(int id) : id(id) {}
-    virtual ~Elemento() {}
-
-    int getID() const {
+    Elemento(int id) : id(id) {}    // Construtor que inicializa o ID. É o que permite a criação de objetos derivados.
+    virtual ~Elemento() {}          // Destrutor virtual.
+   
+    // Método de acesso ao ID — única forma externa de acessar o ID.
+    int getID() const {             
         return id;
     }
 
+    // Método para alteração: permite alterar o ID de um elemento.
     void setID(int novoId) {
         id = novoId;
     }
 
-    // Método virtual puro obriga as classes filhas a implementarem
+    
+    // Método virtual puro — obriga as classes filhas a implementarem sua própria versão.
     virtual void imprimirInfo() const = 0;
+        // Isso faz com que cada tipo de elemento (Aluno e Professor) tenha sua própria forma de imprimir informações,
+        // com isso respeitando o POLIMORFISMO.
 };
 
 #endif
